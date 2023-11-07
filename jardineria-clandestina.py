8e3fda7ac6c08c350e451872beddb7b57d82961c
jadrian-cm <carballojose.im@gmail.com>
2023-11-06 21:41:32 -0600

    Agregué el archivo principal .py

---
+++ jardineria-clandestina.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+
+# Integrantes:
+
+# Pseudoesquema:
+# Menú Inicial: Iniciar juego, instrucciones y solarpunk y jardinería clandestina
+# Menú Dificultad: Fácil, medio, díficil y personalizado
+# Mostrar matriz, que sería la ciudad
+# Menú para las acciones: Planta, Semilla y Ciclovía
+# Actualizar matriz
+# Programar la municipalidad:
+# - Establecer estados
+# - Crear función que verifique el estado de la matriz
+# Crear función para verificar game over
+# Preguntar si se quiere jugar de nuevo
